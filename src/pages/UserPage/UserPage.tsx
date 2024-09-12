a1ba770a31e0506587e82f59c650628f904ae168
sheva3666 <ivan.shevchenko@sap.com>
2024-09-12 18:12:14 +0200

    created user page

---
+++ src/pages/UserPage/UserPage.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useUser from '../../hooks/useUser';
+import { StaticRoutes } from '../../routes';
+
+const User: React.FC = () => {
+  const { user, logout } = useUser();
+  const navigate = useNavigate();
+
+  if (!user) {
+    return <div>Not logged in</div>;
+  }
+
+  const handleLogout = () => {
+    logout();
+    navigate(StaticRoutes.login);
+  };
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default User;
